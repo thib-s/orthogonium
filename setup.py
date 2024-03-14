4f18105a7bbbcb3de35ac168e792889d3fa9ac22
Thibaut Boissin <thibaut.boissin@irt-saintexupery.com>
2024-03-14 17:32:38 +0100

    fix problem with versions

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ if os.path.exists(readme_path):
     with open(readme_path) as fp:
         readme_contents = fp.read().strip()
 
-with open(os.path.join(this_directory, "deel/lipdp/VERSION")) as f:
+with open(os.path.join(this_directory, "flashlipschitz/VERSION")) as f:
     version = f.read().strip()
 
 setup(
