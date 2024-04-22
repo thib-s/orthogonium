673ca8f6d7626a815b292dc8fb92c8765eadca38
Thibaut Boissin <thibaut.boissin@irt-saintexupery.com>
2024-04-22 11:56:53 +0200

    Add GroupMix layer to the flashlipschitz.layers module

--- flashlipschitz/layers/__init__.py
+++ flashlipschitz/layers/__init__.py
@@ -4,6 +4,7 @@ from .custom_activations import HouseHolder_Order_2
 from .custom_activations import MaxMin
 from .fast_block_ortho_conv import FlashBCOP
 from .fast_skew_ortho_conv import SOC
+from .groupmix import GroupMix
 from .normalization import BatchCentering
 from .normalization import LayerCentering
 from .pooling import ScaledAvgPool2d
