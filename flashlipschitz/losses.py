dfd731ae6f0ef700c7ae15615b175457e08e771c
Thibaut Boissin <thibaut.boissin@irt-saintexupery.com>
2024-04-22 11:55:48 +0200

    code cleaning in losses

--- flashlipschitz/losses.py
+++ flashlipschitz/losses.py
@@ -5,17 +5,41 @@ import torch.nn as nn
 
 
 def VRA(output, class_indices, L=1.0, eps=36 / 255, return_certs=False):
+    """Compute the verified robust accuracy (VRA) of a model's output.
+
+    Args:
+        output : torch.Tensor
+            The output of the model.
+        class_indices : torch.Tensor
+            The indices of the correct classes. Should not be one-hot encoded.
+        L : float
+            The Lipschitz constant of the model.
+        eps : float
+            The perturbation size.
+        return_certs : bool
+            Whether to return the certificates instead of the VRA.
+
+    Returns:
+        vra : torch.Tensor
+            The VRA of the model.
+    """
     batch_size = output.shape[0]
     batch_indices = torch.arange(batch_size)
 
+    # get the values of the correct class
+    output_class_indices = output[batch_indices, class_indices]
+    # get the values of the top class that is not the correct class
+    # create a mask indicating the correct class
     onehot = torch.zeros_like(output).cuda()
     onehot[torch.arange(output.shape[0]), class_indices] = 1.0
+    # subtracting a large number from the correct class to ensure it is not the max
+    # doing so will allow us to find the top of the output that is not the correct class
     output_trunc = output - onehot * 1e6
-
-    output_class_indices = output[batch_indices, class_indices]
     output_nextmax = torch.max(output_trunc, dim=1)[0]
+    # now we can compute the certificates
     output_diff = output_class_indices - output_nextmax
     certs = output_diff / (math.sqrt(2) * L)
+    # now we can compute the vra
     # vra is percentage of certs > eps
     vra = (certs > eps).float()
     if return_certs:
