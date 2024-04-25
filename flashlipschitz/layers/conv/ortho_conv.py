ab0df1703b4ae09b552416f847afeab396bf3938
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2024-04-25 23:48:34 +0200

    added composed convolution

---
+++ flashlipschitz/layers/conv/ortho_conv.py
@@ -0,0 +1,148 @@
+from typing import Union
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.utils.parametrize as parametrize
+from torch.nn.common_types import _size_2_t
+
+from flashlipschitz.layers.conv.fast_block_ortho_conv import (
+    attach_bcop_weight,
+    conv_singular_values_numpy,
+    fast_matrix_conv,
+)
+from flashlipschitz.layers.conv.reparametrizers import BjorckParams
+from flashlipschitz.layers.conv.rko_conv import attach_rko_weight
+
+
+class OrthoConv(nn.Conv2d):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: Union[str, _size_2_t] = "same",
+        dilation: _size_2_t = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = "circular",
+        bjorck_params: BjorckParams = BjorckParams(),
+    ):
+        if dilation != 1:
+            raise RuntimeError("dilation not supported")
+        super(OrthoConv, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            groups,
+            bias,
+            padding_mode,
+        )
+        self.padding_mode = padding_mode
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.groups = groups
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.max_channels = max(in_channels, out_channels)
+
+        # raise runtime error if kernel size >= stride
+        if kernel_size < stride:
+            raise RuntimeError(
+                "kernel size must be smaller than stride. The set of orthonal convolutions is empty in this setting."
+            )
+        if (in_channels % groups != 0) and (out_channels % groups != 0):
+            raise RuntimeError(
+                "in_channels and out_channels must be divisible by groups"
+            )
+        if ((self.max_channels // groups) < 2) and (kernel_size != stride):
+            raise RuntimeError("inner conv must have at least 2 channels")
+        self.padding = padding
+        self.stride = stride
+        self.kernel_size = kernel_size
+        self.groups = groups
+        self.intermediate_channels = in_channels
+        # if in_channels >= out_channels * (stride**2):
+        #     self.intermediate_channels = in_channels
+        # else:
+        #     self.intermediate_channels = out_channels * (stride**2)
+        del self.weight
+        attach_bcop_weight(
+            self,
+            "weight_1",
+            (
+                self.intermediate_channels,
+                in_channels // groups,
+                kernel_size - (stride - 1),
+                kernel_size - (stride - 1),
+            ),
+            groups,
+            bjorck_params,
+        )
+        attach_rko_weight(
+            self,
+            "weight_2",
+            (out_channels, self.intermediate_channels // groups, stride, stride),
+            groups,
+            scale=1.0,
+            bjorck_params=bjorck_params,
+        )
+
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_channels))
+            nn.init.zeros_(self.bias)
+        else:
+            self.register_parameter("bias", None)
+
+    @property
+    def weight(self):
+        if self.training:
+            return fast_matrix_conv(self.weight_1, self.weight_2, self.groups)
+        else:
+            with parametrize.cached():
+                return fast_matrix_conv(self.weight_1, self.weight_2, self.groups)
+
+    def singular_values(self):
+        if self.padding_mode != "circular":
+            print(
+                f"padding {self.padding} not supported, return min and max"
+                f"singular values as if it was 'circular' padding "
+                f"(overestimate the values)."
+            )
+        sv_min, sv_max, stable_rank = conv_singular_values_numpy(
+            self.weight_1.detach()
+            .cpu()
+            .reshape(
+                self.groups,
+                self.intermediate_channels // self.groups,
+                self.in_channels // self.groups,
+                self.kernel_size - (self.stride - 1),
+                self.kernel_size - (self.stride - 1),
+            )
+            .numpy(),
+            self._input_shape,
+        )
+        svs_2 = np.linalg.svd(
+            self.weight_2.reshape(
+                self.groups,
+                self.out_channels // self.groups,
+                self.intermediate_channels // self.groups * (self.stride**2),
+            )
+            .detach()
+            .cpu()
+            .numpy(),
+            compute_uv=False,
+        )
+        sv_min = sv_min * svs_2.min()
+        sv_max = sv_max * svs_2.max()
+        stable_rank = 0.5 * stable_rank + 0.5 * (np.mean(svs_2) / (svs_2.max() ** 2))
+        return sv_min, sv_max, stable_rank
+
+    def forward(self, X):
+        self._input_shape = X.shape[2:]
+        return super(OrthoConv, self).forward(X)
