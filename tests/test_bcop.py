405145b0f3feb7e37ce2d6363679451524658aa5
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2024-05-01 23:22:22 +0200

    code factorization in test_bcop

--- tests/test_bcop.py
+++ tests/test_bcop.py
@@ -25,62 +25,33 @@ def _compute_sv_impulse_response_layer(layer, img_shape):
             return torch.norm(outputs.view(outputs.shape[0], -1), p=2).max(), 0, 0
 
 
-@pytest.mark.parametrize("kernel_size", [1, 3, 5])
-@pytest.mark.parametrize("input_channels", [8, 16, 32])
-@pytest.mark.parametrize("output_channels", [16, 32, 64])
-@pytest.mark.parametrize("stride", [1, 2])
-@pytest.mark.parametrize("groups", [1, 2, 4])
-def test_bcop(kernel_size, input_channels, output_channels, stride, groups):
-    # Test instantiation
-    try:
-        bcop = OrthoConv2d(
-            kernel_size=kernel_size,
-            in_channels=input_channels,
-            out_channels=output_channels,
-            stride=stride,
-            groups=groups,
-            bias=False,
-            padding=(kernel_size // 2, kernel_size // 2),
-            padding_mode="circular",
-            bjorck_params=BjorckParams(
-                power_it_niter=3,
-                eps=1e-6,
-                beta=0.5,
-                bjorck_iters=20,
-            ),
-        )
-    except Exception as e:
-        if kernel_size < stride:
-            # we expect this configuration to raise a RuntimeError
-            # pytest.skip(f"BCOP instantiation failed with: {e}")
-            return
-        else:
-            pytest.fail(f"BCOP instantiation failed with: {e}")
+def check_orthogonal_layer(
+    orthoconv,
+    groups,
+    input_channels,
+    kernel_size,
+    output_channels,
+    expected_kernel_shape,
+):
     imsize = 8
     # Test backpropagation and weight update
     try:
-        bcop.train()
-        opt = torch.optim.SGD(bcop.parameters(), lr=0.001)
+        orthoconv.train()
+        opt = torch.optim.SGD(orthoconv.parameters(), lr=0.001)
         for i in range(25):
             opt.zero_grad()
             inp = torch.randn(1, input_channels, imsize, imsize)
-            output = bcop(inp)
+            output = orthoconv(inp)
             loss = -output.mean()
             loss.backward()
             opt.step()
-        bcop.eval()  # so impulse response test checks the eval mode
+        orthoconv.eval()  # so impulse response test checks the eval mode
     except Exception as e:
         pytest.fail(f"Backpropagation or weight update failed with: {e}")
-
-    # check that bcop.weight has the correct shape
-    if bcop.weight.data.shape != (
-        output_channels,
-        input_channels // groups,
-        kernel_size,
-        kernel_size,
-    ):
+    # check that orthoconv.weight has the correct shape
+    if orthoconv.weight.data.shape != expected_kernel_shape:
         pytest.fail(
-            f"BCOP weight has incorrect shape: {bcop.weight.shape} vs {(output_channels, input_channels // groups, kernel_size, kernel_size)}"
+            f"BCOP weight has incorrect shape: {orthoconv.weight.shape} vs {(output_channels, input_channels // groups, kernel_size, kernel_size)}"
         )
     # check that the layer is norm preserving
     inp_norm = torch.sqrt(torch.square(inp).sum(dim=(-3, -2, -1))).float().item()
@@ -89,11 +60,10 @@ def test_bcop(kernel_size, input_channels, output_channels, stride, groups):
         pytest.fail(
             f"BCOP is not norm preserving: {inp_norm} vs {out_norm} with rel error {abs(inp_norm - out_norm) / inp_norm}"
         )
-
     # Test singular_values function
-    sigma_min, sigma_max, stable_rank = bcop.singular_values()  # try:
+    sigma_min, sigma_max, stable_rank = orthoconv.singular_values()  # try:
     sigma_min_ir, sigma_max_ir, stable_rank_ir = _compute_sv_impulse_response_layer(
-        bcop, (input_channels, imsize, imsize)
+        orthoconv, (input_channels, imsize, imsize)
     )
     print(
         f"({input_channels}->{output_channels}, g{groups}, k{kernel_size}), "
@@ -123,3 +93,196 @@ def test_bcop(kernel_size, input_channels, output_channels, stride, groups):
     assert (
         abs(stable_rank - stable_rank_ir) < tol
     ), f"stable_rank is not close to its IR value: {stable_rank} vs {stable_rank_ir}"
+
+
+@pytest.mark.parametrize("kernel_size", [1, 3, 5])
+@pytest.mark.parametrize("input_channels", [8, 16])
+@pytest.mark.parametrize("output_channels", [8, 16])
+@pytest.mark.parametrize("stride", [1, 2])
+@pytest.mark.parametrize("groups", [1, 2, 4])
+def test_standard_configs(kernel_size, input_channels, output_channels, stride, groups):
+    """
+    test combinations of kernel size, input channels, output channels, stride and groups
+    """
+    # Test instantiation
+    try:
+        orthoconv = OrthoConv2d(
+            kernel_size=kernel_size,
+            in_channels=input_channels,
+            out_channels=output_channels,
+            stride=stride,
+            groups=groups,
+            bias=False,
+            padding=(kernel_size // 2, kernel_size // 2),
+            padding_mode="circular",
+            bjorck_params=BjorckParams(
+                power_it_niter=3,
+                eps=1e-6,
+                beta=0.5,
+                bjorck_iters=20,
+            ),
+        )
+    except Exception as e:
+        if kernel_size < stride:
+            # we expect this configuration to raise a RuntimeError
+            # pytest.skip(f"BCOP instantiation failed with: {e}")
+            return
+        else:
+            pytest.fail(f"BCOP instantiation failed with: {e}")
+    check_orthogonal_layer(
+        orthoconv,
+        groups,
+        input_channels,
+        kernel_size,
+        output_channels,
+        (
+            output_channels,
+            input_channels // groups,
+            kernel_size,
+            kernel_size,
+        ),
+    )
+
+
+@pytest.mark.parametrize("kernel_size", [3, 4, 5])
+@pytest.mark.parametrize("input_channels", [2, 4, 8, 16, 32])
+@pytest.mark.parametrize("output_channels", [2, 4, 8, 16, 32])
+@pytest.mark.parametrize("stride", [2, 4])
+@pytest.mark.parametrize("groups", [1])
+def test_strided(kernel_size, input_channels, output_channels, stride, groups):
+    """
+    a more extensive testing when striding is enabled.
+    A larger range of cin and cout is used to track errors when cin < cout / stride**2
+    ( ie you reduce spatial dimensions but you increase the channel dimensions so
+    that you actually increase overall dimension.
+    """
+    # Test instantiation
+    try:
+        orthoconv = OrthoConv2d(
+            kernel_size=kernel_size,
+            in_channels=input_channels,
+            out_channels=output_channels,
+            stride=stride,
+            groups=groups,
+            bias=False,
+            padding=((kernel_size - 1) // 2, (kernel_size - 1) // 2),
+            padding_mode="circular",
+            bjorck_params=BjorckParams(
+                power_it_niter=3,
+                eps=1e-6,
+                beta=0.5,
+                bjorck_iters=20,
+            ),
+        )
+    except Exception as e:
+        if kernel_size < stride:
+            # we expect this configuration to raise a RuntimeError
+            # pytest.skip(f"BCOP instantiation failed with: {e}")
+            return
+        else:
+            pytest.fail(f"BCOP instantiation failed with: {e}")
+    check_orthogonal_layer(
+        orthoconv,
+        groups,
+        input_channels,
+        kernel_size,
+        output_channels,
+        (
+            output_channels,
+            input_channels // groups,
+            kernel_size,
+            kernel_size,
+        ),
+    )
+
+
+@pytest.mark.parametrize("kernel_size", [2, 4])
+@pytest.mark.parametrize("input_channels", [8, 16])
+@pytest.mark.parametrize("output_channels", [8, 16])
+@pytest.mark.parametrize("stride", [1])
+@pytest.mark.parametrize("groups", [1, 2, 4])
+def test_even_kernels(kernel_size, input_channels, output_channels, stride, groups):
+    """
+    test specific to even kernel size
+    """
+    # Test instantiation
+    try:
+        orthoconv = OrthoConv2d(
+            kernel_size=kernel_size,
+            in_channels=input_channels,
+            out_channels=output_channels,
+            stride=stride,
+            groups=groups,
+            bias=False,
+            padding="same",
+            padding_mode="circular",
+            bjorck_params=BjorckParams(
+                power_it_niter=3,
+                eps=1e-6,
+                beta=0.5,
+                bjorck_iters=20,
+            ),
+        )
+    except Exception as e:
+        if kernel_size < stride:
+            # we expect this configuration to raise a RuntimeError
+            # pytest.skip(f"BCOP instantiation failed with: {e}")
+            return
+        else:
+            pytest.fail(f"BCOP instantiation failed with: {e}")
+    check_orthogonal_layer(
+        orthoconv,
+        groups,
+        input_channels,
+        kernel_size,
+        output_channels,
+        (
+            output_channels,
+            input_channels // groups,
+            kernel_size,
+            kernel_size,
+        ),
+    )
+
+
+@pytest.mark.parametrize("kernel_size", [1, 2])
+@pytest.mark.parametrize("input_channels", [4, 8, 16, 32, 64])
+@pytest.mark.parametrize("output_channels", [4, 8, 16, 32, 64])
+@pytest.mark.parametrize("groups", [1, 2])
+def test_rko(kernel_size, input_channels, output_channels, groups):
+    """
+    test case where stride == kernel size
+    """
+    # Test instantiation
+    try:
+        rkoconv = OrthoConv2d(
+            kernel_size=kernel_size,
+            in_channels=input_channels,
+            out_channels=output_channels,
+            stride=kernel_size,
+            groups=groups,
+            bias=False,
+            padding=(0, 0),
+            padding_mode="zeros",
+            bjorck_params=BjorckParams(
+                power_it_niter=3,
+                eps=1e-6,
+                beta=0.5,
+                bjorck_iters=20,
+            ),
+        )
+    except Exception as e:
+        pytest.fail(f"BCOP instantiation failed with: {e}")
+    check_orthogonal_layer(
+        rkoconv,
+        groups,
+        input_channels,
+        kernel_size,
+        output_channels,
+        (
+            output_channels,
+            input_channels // groups,
+            kernel_size,
+            kernel_size,
+        ),
+    )
